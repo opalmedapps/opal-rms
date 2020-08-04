7ca8097bc24e93822bf508a1df6d4969a5e52313
vmatassa <victor.matassa@muhc.mcgill.ca>
2020-08-04 10:17:09 -0400

    Updated sms registration to use messages in db

--- VirtualWaitingRoom/php/setPatientMobileNumber.php
+++ VirtualWaitingRoom/php/setPatientMobileNumber.php
@@ -70,14 +70,8 @@ $querySMS->execute([
 echo "Record updated successfully<br>";
 
 #change the sms message depending on the language preference and clinic
-if($languagePreference === "French")
-{
-    $message = "CUSM: l'inscription pour les notifications est confirmée. Pour vous désabonner, veuillez informer la réception. Pour enregistrer pour un rendez-vous, répondez à ce numéro avec \"arrive\".";
-}
-else
-{
-    $message = "MUHC: You are registered for SMS notifications. To unsubscribe, please inform the reception. To check-in for an appointment, reply to this number with \"arrive\".";
-}
+$messageList = getPossibleSmsMessages();
+$message = $messageList["Oncology"]["GENERAL"]["REGISTRATION"][$languagePreference]["Message"];
 
 #send sms
 $SMS_licencekey = SMS_licencekey;
@@ -100,5 +94,88 @@ curl_setopt_array($curl,[
     CURLOPT_HTTPHEADER      => ["Content-Type: application/json","Accept: application/json"]
 ]);
 curl_exec($curl);
+
+
+
+function getPossibleSmsMessages(): array
+{
+    $dbh = Config::getDatabaseConnection("ORMS");
+    $query = $dbh->prepare("
+        SELECT
+            Speciality
+            ,Type
+            ,Event
+            ,Language
+            ,Message
+        FROM
+            SmsMessage
+        ORDER BY
+            Speciality,Type,Event,Language
+    ");
+    $query->execute();
+
+    $messages = $query->fetchAll();
+    $messages = ArrayUtilB::groupArrayByKeyRecursive($messages,"Speciality","Type","Event","Language");
+    $messages = ArrayUtilB::convertSingleElementArraysRecursive($messages);
+
+    return utf8_encode_recursive($messages);
+}
+
+class ArrayUtilB
+{
+    public static function groupArrayByKey(array $arr,string $key,bool $keepKey = FALSE): array
+    {
+        $groupedArr = [];
+        foreach($arr as $assoc)
+        {
+            $keyVal = $assoc[$key];
+            if(!array_key_exists("$keyVal",$groupedArr)) $groupedArr["$keyVal"] = [];
+
+            if($keepKey === FALSE) unset($assoc[$key]);
+            $groupedArr["$keyVal"][] = $assoc;
+        }
+
+        ksort($groupedArr);
+        return $groupedArr;
+    }
+
+    #recursive version of groupArrayByKey that repeats the grouping process for each input key
+    public static function groupArrayByKeyRecursive(array $arr,string ...$keys): array
+    {
+        $key = array_shift($keys);
+        if($keys === NULL) return $arr;
+
+        $groupedArr = self::groupArrayByKey($arr,"$key");
+
+        if($keys !== [])
+        {
+            foreach($groupedArr as &$subArr) {
+                $subArr = self::groupArrayByKeyRecursive($subArr,...$keys);
+            }
+        }
+
+        return $groupedArr;
+    }
+
+    public static function convertSingleElementArraysRecursive($arr)
+    {
+        if(gettype($arr) === "array")
+        {
+            foreach($arr as &$val) $val = self::convertSingleElementArraysRecursive($val);
+
+            if(self::checkIfArrayIsAssoc($arr) === FALSE && count($arr) === 1) {
+                $arr = $arr[0];
+            }
+        }
+
+        return $arr;
+    }
+
+    public static function checkIfArrayIsAssoc(array $arr): bool
+    {
+        return array_keys($arr) !== range(0,count($arr)-1);
+    }
+
+}
 
 ?>

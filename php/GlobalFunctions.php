4d0d92c3d5f056a556e6b8aaaeae5b5daa355e5e
Victor Matassa <victor.matassa@muhc.mcgill.ca>
2020-11-10 17:42:09 -0500

    php errors are now saved to a log file

--- php/GlobalFunctions.php
+++ php/GlobalFunctions.php
@@ -4,6 +4,7 @@
 #########################################
 
 error_reporting(E_ALL);
+ini_set("error_log", __DIR__."/../php-error.log");
 date_default_timezone_set("America/Montreal");
 
 function getPostContents()

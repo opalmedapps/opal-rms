8db20478edfb619683f0508f3642d46b7fe95dbb
vmatassa <victor.matassa@muhc.mcgill.ca>
2021-05-20 01:51:25 -0400

    Reorganized migration scripts

---
+++ tools/databaseMaintenance/multisite/removeLegacy/updateProfiles.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types = 1);
+
+require_once __DIR__ ."/../../../../vendor/autoload.php";
+
+function removeLegacyProfileColumns(PDO $dbh): void
+{
+    $dbh->query("
+        ALTER TABLE `Profile`
+        DROP COLUMN `FetchResourcesFromVenues`,
+        DROP COLUMN `FetchResourcesFromClinics`,
+        DROP COLUMN `ShowCheckedOutAppointments`;
+    ;");
+}
+
+?>

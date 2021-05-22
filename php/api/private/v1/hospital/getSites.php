55a856cfbd981868c00c545928987546d31c48a8
Victor Matassa <victor.matassa@muhc.mcgill.ca>
2021-05-22 01:04:12 -0400

    Patient finder page now has site dropdown

---
+++ php/api/private/v1/hospital/getSites.php
@@ -0,0 +1,19 @@
+<?php declare(strict_types=1);
+
+require __DIR__."/../../../../../vendor/autoload.php";
+
+use Orms\Http;
+use Orms\Database;
+
+$dbh = Database::getOrmsConnection();
+
+$query = $dbh->prepare("
+    SELECT
+        HospitalCode,
+        HospitalName
+    FROM
+        Hospital
+");
+$query->execute();
+
+Http::generateResponseJsonAndExit(200,data: $query->fetchAll());

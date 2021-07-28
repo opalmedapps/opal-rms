51670dc5ad9c0501784501c6c7fd2fb7e7a0777e
vmatassa <victor.matassa@muhc.mcgill.ca>
2021-07-27 20:33:46 -0400

    Added php-cs-fixer tool

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,22 @@
+<?php declare(strict_types=1);
+
+$config = new PhpCsFixer\Config();
+
+return $config->setRules([
+    //"@PSR12" => true,
+    // "visibility_required" => [],
+    // "declare_strict_types" => true,
+    "strict_comparison" => true,
+    // "function_declaration" => [
+    //     "closure_function_spacing" => "none"
+    // ],
+    // "mb_str_functions" => true,
+    // "dir_constant" => true
+])
+->setRiskyAllowed(true)
+->setCacheFile(__DIR__."/vendor/php-cs-fixer/.php-cs-fixer.cache")
+->setFinder(PhpCsFixer\Finder::create()
+    ->in(__DIR__)
+    ->exclude("vendor")
+    ->exclude("VirtualWaitingRoom/php/pdfGenerator")
+);

90d9d751c041e9af46847c669bf521ffcab8f15a
Victor Matassa <victor.matassa@muhc.mcgill.ca>
2021-08-04 16:29:34 -0400

    Updated cs-fixer with double quite fixer

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,8 +1,76 @@
 <?php declare(strict_types=1);
 
+use PhpCsFixer\Fixer\FixerInterface;
+use PhpCsFixer\FixerDefinition\CodeSample;
+use PhpCsFixer\FixerDefinition\FixerDefinition;
+use PhpCsFixer\FixerDefinition\FixerDefinitionInterface;
+use PhpCsFixer\Tokenizer\Tokens;
+use PhpCsFixer\Tokenizer\Token;
+
+class DoubleQuoteFixer implements FixerInterface
+{
+    public function getName(): string
+    {
+        return "Orms/double_quote_fixer";
+    }
+
+    public function isRisky(): bool
+    {
+        return true;
+    }
+
+    public function getPriority(): int
+    {
+        return 0;
+    }
+
+    public function supports(SplFileInfo $file): bool
+    {
+        return true;
+    }
+
+    public function getDefinition(): FixerDefinitionInterface
+    {
+        return new FixerDefinition("aaa",[new CodeSample("'aaa' => \"aaa\"")]);
+    }
+
+    public function isCandidate(Tokens $tokens): bool
+    {
+        return $tokens->isTokenKindFound(T_CONSTANT_ENCAPSED_STRING);
+    }
+
+    // foreach string that starts with single quotes, convert it to double quotes
+    // ignore any strings that have single quotes inside of them
+    public function fix(\SplFileInfo $file, Tokens $tokens): void
+    {
+        foreach ($tokens as $index => $token)
+        {
+            if (!$token->isGivenKind(T_CONSTANT_ENCAPSED_STRING)) {
+                continue;
+            }
+
+            $content = $token->getContent();
+            if (
+                $content[0] === "'"
+                && $content[strlen($content) -1] === "'"
+                && str_contains($content,"\"") === false
+            ) {
+                $content = substr($content,1,-1);
+                // $content = preg_replace("/\"/","\\\"",$content); //replace all double quotes in the string with an escaped double quote
+                $content = "\"$content\"";
+
+                $tokens[$index] = new Token($content);
+            }
+        }
+    }
+}
+
 $config = new PhpCsFixer\Config();
 
-return $config->setRules([
+return $config->registerCustomFixers([
+    new DoubleQuoteFixer()
+])
+->setRules([
     //"@PSR12" => true,
     // "visibility_required" => [],
     "declare_strict_types" => true,
@@ -11,7 +79,8 @@ return $config->setRules([
     //     "closure_function_spacing" => "none"
     // ],
     // "mb_str_functions" => true,
-    "dir_constant" => true
+    "dir_constant" => true,
+    //"Orms/double_quote_fixer" => true
 ])
 ->setRiskyAllowed(true)
 ->setCacheFile(__DIR__."/vendor/php-cs-fixer/.php-cs-fixer.cache")

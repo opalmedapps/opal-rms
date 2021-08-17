7812165c9411245aed04ba173b4dd385e18160a3
vmatassa <victor.matassa@muhc.mcgill.ca>
2021-08-17 16:55:24 -0400

    Changed some api paths to get

--- php/class/Http.php
+++ php/class/Http.php
@@ -4,13 +4,12 @@ declare(strict_types=1);
 
 namespace Orms;
 
-use cebe\openapi\Reader;
-use cebe\openapi\spec\OpenApi;
 use Exception;
-use League\OpenAPIValidation\Schema\Exception\InvalidSchema;
+use GuzzleHttp\Psr7\ServerRequest;
+use GuzzleHttp\Psr7\Utils;
+use League\OpenAPIValidation\PSR7\Exception\Validation\InvalidBody;
+use League\OpenAPIValidation\PSR7\ValidatorBuilder;
 use League\OpenAPIValidation\Schema\Exception\KeywordMismatch;
-use League\OpenAPIValidation\Schema\SchemaValidator;
-use Respect\Validation\Exceptions\ValidatorException;
 
 class Http
 {
@@ -71,46 +70,43 @@ class Http
     public static function parseApiInputs(): array
     {
         //get from config
-        $publicApiPath = "/var/www/OnlineRoomManagementSystem/php/api/public/v1";
+        $basePath = Config::getApplicationSettings()->environment->basePath;
+        $apiSpecFile = "$basePath/php/api/public/v1/openapi.yml";
 
-        $specification = Reader::readFromYamlFile(
-            fileName: "$publicApiPath/openapi.yml",
-            baseType: OpenApi::class,
-            resolveReferences: true
-        );
-
-        //get the specification section for the api being called
-        $apiPath = str_replace([$publicApiPath,".php"], "", $_SERVER["SCRIPT_FILENAME"] ?? "");
+        //get the specifications for the api being called
+        $apiPath = str_replace([$basePath,".php"], "", $_SERVER["SCRIPT_FILENAME"] ?? "");
         $method = mb_strtolower($_SERVER["REQUEST_METHOD"] ?? "");
-        $content = preg_replace("/;\s?charset=.+$/", "", $_SERVER["CONTENT_TYPE"] ?? ""); //remove the charset
+        $contentType = preg_replace("/;\s?charset=.+$/", "", $_SERVER["CONTENT_TYPE"] ?? ""); //remove the charset
 
-        $specification = $specification->paths[$apiPath] ?? throw new Exception("Unknown api");
-        $specification = $specification->$method->requestBody ?? throw new Exception("Unknown method");
-        $specification = $specification->content[$content] ?? throw new Exception("Unknown content type");
+        //parse the request inputs and ensure they conform to the api spec
+        if($method === "get") {
+            $inputs = self::sanitizeRequestParams($_GET);
+            $request = (new ServerRequest($method,$apiPath))->withQueryParams($inputs);
+        }
+        else {
+            $inputs = self::getRequestContents();
+            $request = (new ServerRequest($method,$apiPath))->withHeader("Content-Type",$contentType)->withBody(Utils::streamFor(json_encode($inputs)));
+        }
 
-        $input = self::getRequestContents();
+        $validator = (new ValidatorBuilder())->fromYamlFile($apiSpecFile)->getServerRequestValidator();
+        /** @psalm-suppress ArgumentTypeCoercion */ //bug with psalm; it can't properly detect a static return type
+        $validator->validate($request);
 
-        //parse the inputs and ensure they conform to the api spec
-        (new SchemaValidator(SchemaValidator::VALIDATE_AS_REQUEST))->validate($input, $specification->schema);
-
-        return $input;
+        return $inputs;
     }
 
     public static function generateApiParseError(Exception $e): string
     {
         //various errors can be returned from the parser
         //handle all of them and decompose the errors if needed
-        if($e instanceof KeywordMismatch) {
-            $failedFields = $e->dataBreadCrumb()?->buildChain() ?? [];
-            $failedString = implode("' -> '", $failedFields);
-            $errorString = "Field '$failedString': {$e->getMessage()}";
-        }
-        elseif($e instanceof InvalidSchema) {
 
-            $trueError = $e->getPrevious();
+        if($e instanceof InvalidBody) {
+            $prevErr = $e->getPrevious();
 
-            if($trueError instanceof ValidatorException) {
-                $errorString = $trueError->getFullMessage();
+            if($prevErr instanceof KeywordMismatch) {
+                $failedFields = $prevErr->dataBreadCrumb()?->buildChain() ?? [];
+                $failedString = implode("' -> '", $failedFields);
+                $errorString = "Field '$failedString': {$prevErr->getMessage()}";
             }
             else {
                 $errorString = $e->getMessage();

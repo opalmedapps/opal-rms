e9debf52378ff7e9e22c9186806a94b4a114a3f3
Victor Matassa <victor.matassa@muhc.mcgill.ca>
2021-08-21 01:37:10 -0400

    Fixed type bug for sms appointment update

--- php/class/ApplicationException.php
+++ php/class/ApplicationException.php
@@ -8,21 +8,22 @@ use Exception;
 
 class ApplicationException extends Exception
 {
-    public const UNKNOWN_INSURANCE_TYPE = 1;
-    public const INVALID_INSURANCE_FORMAT = 2;
-    public const UNKNOWN_MRN_TYPE = 3;
-    public const INVALID_MRN_FORMAT = 4;
-    public const INVALID_SMS_APPOINTMENT_STATE = 5;
-    public const NO_ACTIVE_MRNS = 6;
+    public const UNKNOWN_INSURANCE_TYPE = "UNKNOWN_INSURANCE_TYPE";
+    public const INVALID_INSURANCE_FORMAT = "INVALID_INSURANCE_FORMAT";
+    public const UNKNOWN_MRN_TYPE = "UNKNOWN_MRN_TYPE";
+    public const INVALID_MRN_FORMAT = "INVALID_MRN_FORMAT";
+    public const NO_ACTIVE_MRNS = "NO_ACTIVE_MRNS";
+    public const INVALID_SMS_APPOINTMENT_STATE = "INVALID_SMS_APPOINTMENT_STATE";
+    public const INVALID_SMS_APPOINTMENT_TYPE ="INVALID_SMS_APPOINTMENT_TYPE";
 
-    public function __construct(int $errorCode, string $errorMessage)
+    public function __construct(string $errorCode, string $errorMessage)
     {
         $message = self::getConstantString($errorCode) .": $errorMessage";
 
         parent::__construct($message);
     }
 
-    private static function getConstantString(int $code): string
+    private static function getConstantString(string $code): string
     {
         $class = new \ReflectionClass(__CLASS__);
         $constants = array_flip($class->getConstants());

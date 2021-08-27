71bcda1130d908a209ef1f67ad9fe553dc3bdf6a
vmatassa <victor.matassa@muhc.mcgill.ca>
2021-08-27 08:13:05 -0400

    Fixed urls

--- php/class/Hospital/OIE/Internal/Connection.php
+++ php/class/Hospital/OIE/Internal/Connection.php
@@ -14,16 +14,16 @@ class Connection
     public const API_APPOINTMENT_COMPLETION             = "Appointment/Status";
     public const API_MEASUREMENT_PDF                    = "report/post";
     public const API_PATIENT_QUESTIONNAIRE_COMPLETED    = "questionnaire/get/questionnaires-list-orms";
-    public const API_PATIENT_QUESTIONNAIRE_LAST         = "opalAdmin/questionnaire/get/last-completed-quesitonnaire";
+    public const API_PATIENT_QUESTIONNAIRE_LAST         = "questionnaire/get/last-completed-quesitonnaire";
     public const API_PATIENT_QUESTIONNAIRE_PUBLISHED    = "questionnaire/get/published-questionnaires";
     public const API_PATIENT_DIAGNOSIS                  = "Patient/Diagnosis";
     public const API_PATIENT_FETCH                      = "patient/get";
     public const API_PATIENT_LOCATION                   = "Patient/Location";
-    public const API_QUESTIONNAIRE_ANSWERS_CHART        = "opalAdmin/questionnaire/get/chart-answers-patient";
-    public const API_QUESTIONNAIRE_ANSWERS_NON_CHART    = "opalAdmin/questionnaire/get/non-chart-answers-patient";
-    public const API_QUESTIONNAIRE_PATIENT_COMPLETED    = "opalAdmin/questionnaire/get/patients-completed-questionaires";
-    public const API_QUESTIONNAIRE_PURPOSE              = "opalAdmin/questionnaire/get/purposes";
-    public const API_QUESTIONNAIRE_STUDY                = "/opalAdmin/study/get/studies-patient-consented";
+    public const API_QUESTIONNAIRE_ANSWERS_CHART        = "questionnaire/get/chart-answers-patient";
+    public const API_QUESTIONNAIRE_ANSWERS_NON_CHART    = "questionnaire/get/non-chart-answers-patient";
+    public const API_QUESTIONNAIRE_PATIENT_COMPLETED    = "questionnaire/get/patients-completed-questionaires";
+    public const API_QUESTIONNAIRE_PURPOSE              = "questionnaire/get/purposes";
+    public const API_QUESTIONNAIRE_STUDY                = "study/get/studies-patient-consented";
     public const API_ROOM_NOTIFICATION                  = "Patient/RoomNotification";
 
     public static function getHttpClient(): ?Client

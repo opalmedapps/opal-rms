d9d66a645a5ad462a0fc460b0d015815db863ef1
Victor Matassa <victor.matassa@muhc.mcgill.ca>
2021-08-28 08:44:59 -0400

    Moved logging queries

--- php/class/DataAccess/LoggerAccess.php
+++ php/class/DataAccess/LoggerAccess.php
@@ -9,6 +9,32 @@ use Orms\DataAccess\Database;
 
 class LoggerAccess
 {
+    /**
+     *
+     * @return list<array{
+     *  messageId: string,
+     *  timestamp: string
+     * }>
+     */
+    public static function getLoggedSmsEvent(string $smsMessageId): array
+    {
+        $query = Database::getLogsConnection()->prepare("
+            SELECT
+                MessageId,
+                SmsTimestamp
+            FROM
+                SmsLog
+            WHERE
+                MessageId = ?
+        ");
+        $query->execute([$smsMessageId]);
+
+        return array_map(fn($x) => [
+            "messageId" => $x["MessageId"],
+            "timestamp" => $x["SmsTimestamp"]
+        ],$query->fetchAll());
+    }
+
     public static function insertSmsEvent(
         string $clientNumber,
         string $serviceNumber,
@@ -22,24 +48,24 @@ class LoggerAccess
     {
         Database::getLogsConnection()->prepare("
             INSERT INTO SmsLog(
-                SmsTimestamp
-                ,Result
-                ,Action
-                ,Service
-                ,MessageId
-                ,ServicePhoneNumber
-                ,ClientPhoneNumber
-                ,Message
+                SmsTimestamp,
+                Result,
+                Action,
+                Service,
+                MessageId,
+                ServicePhoneNumber,
+                ClientPhoneNumber,
+                Message
             )
             VALUES(
-                :timestamp
-                ,:result
-                ,:action
-                ,:service
-                ,:messageId
-                ,:serviceNum
-                ,:clientNum
-                ,:message
+                :timestamp,
+                :result,
+                :action,
+                :service,
+                :messageId,
+                :serviceNum,
+                :clientNum,
+                :message
             )
         ")->execute([
             ":timestamp"   => $timestamp->format("Y-m-d H:i:s"),
@@ -50,6 +76,32 @@ class LoggerAccess
             ":serviceNum"  => $serviceNumber,
             ":clientNum"   => $clientNumber,
             ":message"     => $message
+        ]);
+    }
+
+    public static function logVwrEvent(string $filename,string $identifier, string $type, string $message): void
+    {
+        Database::getLogsConnection()->prepare("
+            INSERT INTO VirtualWaitingRoomLog(
+                DateTime,
+                FileName,
+                Identifier,
+                Type,
+                Message
+            )
+            VALUES(
+                :date,
+                :file,
+                :id,
+                :type,
+                :message
+            )
+        ")->execute([
+            ":date"     => (new DateTime())->format("Y-m-d H:i:s"),
+            ":file"     => $filename,
+            ":id"       => $identifier,
+            ":type"     => $type,
+            ":message"  => $message,
         ]);
     }
 }

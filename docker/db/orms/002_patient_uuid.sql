297d0513f76513726399b8d23766cbb8a1a3d1d8
Stacey Beard <staceybeard10@gmail.com>
2023-02-27 15:12:17 -0500

    Add alter statement to create Patient UUID column

---
+++ docker/db/orms/002_patient_uuid.sql
@@ -0,0 +1,10 @@
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+SET @@system_versioning_alter_history = 1;
+
+ALTER TABLE `Patient`
+    ADD COLUMN `OpalUUID` VARCHAR(36) NOT NULL DEFAULT '' COMMENT 'UUID provided only for Opal patients, and received from Opal' COLLATE 'latin1_swedish_ci' AFTER `OpalPatient`;
+
+SET @@system_versioning_alter_history = 0;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

5aa5677620e79b7ae25a7811f5f62e824cf8cc21
Dan Howard <daniel.ls.howard@gmail.com>
2023-08-09 15:21:07 +0000

    FIX: Expand try catch to cover complete process
    See merge request opalmedapps/ORMS!98

--- php/api/public/v1/appointment/insertClinicResource.php
+++ php/api/public/v1/appointment/insertClinicResource.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-require __DIR__."/../../../../../vendor/autoload.php";
+require __DIR__ . "/../../../../../vendor/autoload.php";
 
 use Orms\Appointment\AppointmentInterface;
 use Orms\Http;
@@ -12,38 +12,38 @@ try {
     $fields = Http::parseApiInputs('v1');
     $fields = Http::sanitizeRequestParams($fields);
     $fields = Encoding::utf8_decode_recursive($fields);
-}
-catch(\Exception $e) {
+
+    $resource = new class(
+        appointmentCode: $fields["appointmentCode"],
+        clinics: $fields["clinics"],
+        sourceSystem: $fields["sourceSystem"],
+        specialityGroupCode: $fields["specialityGroupCode"],
+    ) {
+        public string $clinicCode;
+        public string $clinicDescription;
+
+        /** @param mixed[] $clinics */
+        public function __construct(
+            public string $appointmentCode,
+            array         $clinics,
+            public string $sourceSystem,
+            public string $specialityGroupCode,
+        ) {
+            $this->clinicCode = implode("; ", array_map(fn($x) => $x["clinicCode"], $clinics));
+            $this->clinicDescription = implode("; ", array_map(fn($x) => $x["clinicDescription"], $clinics));
+        }
+    };
+
+    AppointmentInterface::insertClinicResource(
+        appointmentCode: $resource->appointmentCode,
+        clinicCode: $resource->clinicCode,
+        clinicDescription: $resource->clinicDescription,
+        specialityGroupCode: $resource->specialityGroupCode,
+        system: $resource->sourceSystem,
+    );
+
+} catch (\Exception $e) {
     Http::generateResponseJsonAndExit(400, error: Http::generateApiParseError($e));
 }
-
-$resource = new class(
-    appointmentCode:        $fields["appointmentCode"],
-    clinics:                $fields["clinics"],
-    sourceSystem:           $fields["sourceSystem"],
-    specialityGroupCode:    $fields["specialityGroupCode"],
-) {
-    public string $clinicCode;
-    public string $clinicDescription;
-
-    /** @param mixed[] $clinics */
-    public function __construct(
-        public string $appointmentCode,
-        array $clinics,
-        public string $sourceSystem,
-        public string $specialityGroupCode,
-    ) {
-        $this->clinicCode = implode("; ", array_map(fn($x) => $x["clinicCode"], $clinics));
-        $this->clinicDescription = implode("; ", array_map(fn($x) => $x["clinicDescription"], $clinics));
-    }
-};
-
-AppointmentInterface::insertClinicResource(
-    appointmentCode:        $resource->appointmentCode,
-    clinicCode:             $resource->clinicCode,
-    clinicDescription:      $resource->clinicDescription,
-    specialityGroupCode:    $resource->specialityGroupCode,
-    system:                 $resource->sourceSystem,
-);
 
 Http::generateResponseJsonAndExit(200);

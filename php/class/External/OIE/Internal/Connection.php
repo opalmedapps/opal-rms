2624abc76fe32ece54a499ba27550b3c4bfa2568
Napparific <kelly.agnew@mail.mcgill.ca>
2024-08-21 11:07:30 -0400

    adjust connection naming and remove defunct code

--- php/class/External/OIE/Internal/Connection.php
+++ php/class/External/OIE/Internal/Connection.php
@@ -16,7 +16,6 @@ class Connection
     public const API_MEASUREMENT_PDF                            = "report/post";
     public const API_PATIENT_DIAGNOSIS                          = "Patient/Diagnosis";
     public const API_PATIENT_LOCATION                           = "Patient/Location";
-    public const API_PATIENT_QUESTIONNAIRE_ANSWERS              = "Patient/Questionnaire/Answer";
     public const API_ROOM_NOTIFICATION                          = "Patient/RoomNotification";
 
     public static function getHttpClient(): ?Client

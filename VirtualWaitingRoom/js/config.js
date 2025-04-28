38b7c22fb3b6d57ca71fa90d67239f7be7e6718f
Zeyu Dou <dou3180@hotmail.com>
2025-04-28 15:35:56 -0400

    tmp fix for demo

--- VirtualWaitingRoom/js/config.js
+++ VirtualWaitingRoom/js/config.js
@@ -8,11 +8,11 @@
     angular
       .module('vwr.config', [])
       .constant('CONFIG', {
-        BRANDING_APP_LOGO_PATH: '{your_app_logo_path}',
-        BRANDING_RMS_LOGO_PATH: 'images/BRANDING_your_logo_here.png',
+        BRANDING_APP_LOGO_PATH: 'VirtualWaitingRoom/images/Opal_logo.png',
+        BRANDING_RMS_LOGO_PATH: 'images/Opal_RMS_logo.png',
         BRANDING_MOBILE_APP_NAME: '{APP_NAME}',
-        BRANDING_SCREEN_DISPLAY_BACKGROUND_PATH: 'VirtualWaitingRoom/images/BRANDING_customize_watermark_background.png',
-        BRANDING_SCREEN_DISPLAY_BANNER_PATH: 'VirtualWaitingRoom/images/BRANDING_banner_your_product_name.png',
+        BRANDING_SCREEN_DISPLAY_BACKGROUND_PATH: 'VirtualWaitingRoom/images/background.jpg',
+        BRANDING_SCREEN_DISPLAY_BANNER_PATH: 'VirtualWaitingRoom/images/Banner_treatments.png',
         BRANDING_SUPPORT_EMAIL_ADDRESS: '{BRANDING_SUPPORT_EMAIL_ADDRESS}'
       });
   

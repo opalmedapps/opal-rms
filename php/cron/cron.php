74704c7d4ae007310bf28eec9c40c47c4daa06ed
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-05-15 19:29:12 +0000

    Added other crons
    (cherry picked from commit d6d2157771d931681b33d5ae4425d6276d104dba)
    
    Co-authored-by: vmatassa <victor.matassa@muhc.mcgill.ca>

--- php/cron/cron.php
+++ php/cron/cron.php
@@ -5,24 +5,54 @@ require_once __DIR__."/../../vendor/autoload.php";
 use Crunz\Schedule;
 use Orms\Config;
 
+//normally, the config file is loaded once and used throughout the request lifespan
+//in this case, some crons will stay alive indefinitely and need the current state of the config file
+$reloadConfigs = function(): Config
+{
+    Config::__init();
+    return Config::getApplicationSettings();
+};
+
 $configs = Config::getApplicationSettings();
 $schedule = new Schedule();
 
-$currentAppointmentsTask = $schedule->run(function() use ($configs) {
-
-    $x = $configs->system->vwrAppointmentCronEnabled;
-    // file_put_contents("aaa.txt",var_dump($configs->system->vwrAppointmentCronEnabled,true),FILE_APPEND);
-
-    // while($configs->system->vwrAppointmentCronEnabled === true) {
-    //     file_put_contents("aaa.txt","aaa: " .$configs->system->vwrAppointmentCronEnabled."\n",FILE_APPEND);
-    //     sleep(5);
-    // }
+$currentAppointmentsTask = $schedule->run(function() use ($configs,$reloadConfigs) {
+    while($configs->system->vwrAppointmentCronEnabled === true)
+    {
+        $configs = $reloadConfigs();
+        /** @psalm-suppress ForbiddenCode */
+        `php ../php/cron/generateVwrAppointments.php`;
+        sleep(3);
+    }
 })
 ->when(fn() => $configs->system->vwrAppointmentCronEnabled === true)
 ->everyMinute()
 ->preventOverlapping()
 ->timezone(date_default_timezone_get())
 ->description("Virtual waiting room appointment file generator");
+
+$incomingSmsTask = $schedule->run(function() use ($configs,$reloadConfigs) {
+    while($configs->system->processIncomingSmsCronEnabled === true)
+    {
+        $configs = $reloadConfigs();
+        /** @psalm-suppress ForbiddenCode */
+        `php ../php/cron/processIncomingSmsMessages.php`;
+        sleep(5);
+    }
+})
+->when(fn() => $configs->system->processIncomingSmsCronEnabled === true)
+->everyMinute()
+->preventOverlapping()
+->timezone(date_default_timezone_get())
+->description("Incoming sms processor");
+
+$appointmentReminderTask = $schedule->run("php ../php/cron/generateAppointmentReminders.php")
+->when(fn() => $configs->system->appointmentReminderCronEnabled === true)
+->daily()
+->hour(18)
+->preventOverlapping()
+->timezone(date_default_timezone_get())
+->description("Appointment sms reminder");
 
 
 return $schedule;

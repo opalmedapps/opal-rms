dcdea81a606096dfbeb9c1044cb4813ee0ea5a28
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-05-15 19:41:50 +0000

    env variable renaming and other nits
    (cherry picked from commit 8a2ed2367b5631e7f48000630b04dd302dd90b40)
    
    Co-authored-by: Napparific <kelly.agnew@mail.mcgill.ca>

--- php/api/private/v1/questionnaire/getPurposes.php
+++ php/api/private/v1/questionnaire/getPurposes.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 require_once __DIR__."/../../../../../vendor/autoload.php";
 
-use Orms\External\LEGACY_OA\Fetch;
+use Orms\External\LegacyOpalAdmin\Fetch;
 use Orms\Http;
 
 $purposes = Fetch::getQuestionnairePurposes();

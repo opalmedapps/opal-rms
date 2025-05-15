e355858ffd4723fe65d36ac62e0ea6099d4364c6
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-05-15 19:42:38 +0000

    fix recursive utf8 coding functions to avoid double encoding issue
    (cherry picked from commit ecb646a6e46314ff4883a96692df7e56dbb6fe89)
    
    Co-authored-by: Napparific <kelly.agnew@mail.mcgill.ca>

--- php/class/Util/Encoding.php
+++ php/class/Util/Encoding.php
@@ -14,13 +14,18 @@ class Encoding
     public static function utf8_encode_recursive(mixed $data): mixed 
     {
         if (is_string($data)) {
-            return mb_convert_encoding($data, 'UTF-8', 'ISO-8859-1');
+            // Check if the string is already UTF-8 to avoid double encoding characters
+            if (!mb_check_encoding($data, 'UTF-8')) {
+                // Convert from ISO-8859-1 to UTF-8 if it’s not UTF-8
+                return mb_convert_encoding($data, 'UTF-8', 'ISO-8859-1');
+            }
+            return $data; // Return as is if it's already UTF-8
         } 
         elseif (is_array($data)) {
             return array_map(fn($x) => self::utf8_encode_recursive($x), $data);
         }
 
-        return $data;
+        return $data; // Return as is for non-string, non-array types
     }
 
     /**
@@ -31,7 +36,12 @@ class Encoding
     public static function utf8_decode_recursive(mixed $data): mixed 
     {
         if (is_string($data)) {
-            return mb_convert_encoding($data, 'ISO-8859-1', 'UTF-8');
+            // Check if the string is already ISO-8859-1
+            if (!mb_check_encoding($data, 'ISO-8859-1')) {
+                // Convert from UTF-8 to ISO-8859-1 if it’s not ISO-8859-1
+                return mb_convert_encoding($data, 'ISO-8859-1', 'UTF-8');
+            }
+            return $data; // Return as is if it's already ISO-8859-1
         } 
         elseif (is_array($data)) {
             return array_map(fn($x) => self::utf8_decode_recursive($x), $data);

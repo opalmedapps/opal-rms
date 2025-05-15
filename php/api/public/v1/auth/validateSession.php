24e8d62e76a93ac4ce388687ad90c9ddfc9ea4b5
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-05-15 19:41:59 +0000

    fix endpoint url
    (cherry picked from commit 3d9a1cf71b3f4620ece1840b8c96b24bad3983f1)
    
    Co-authored-by: Napparific <kelly.agnew@mail.mcgill.ca>

--- php/api/public/v1/auth/validateSession.php
+++ php/api/public/v1/auth/validateSession.php
@@ -18,7 +18,7 @@ use Orms\System\Logger;
 
 
 try {
-    $opalAdminHost = Config::getApplicationSettings()->environment->opalAdminHost;
+    $opalAdminHost = Config::getApplicationSettings()->environment->legacyOpalAdminHostExternal;
     $sessionid = isset($_COOKIE["sessionid"]) ? $_COOKIE["sessionid"] : "";
 
     $response = Authentication::validate($sessionid);

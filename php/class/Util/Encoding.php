986235a2c63862a48df14fd233dbdf77b91ab028
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-05-15 19:40:46 +0000

    update image version and encoding functions
    (cherry picked from commit c16b1e89d8b7af180321e028f388a7e9cac62fa0)
    
    Co-authored-by: Napparific <kelly.agnew@mail.mcgill.ca>

--- php/class/Util/Encoding.php
+++ php/class/Util/Encoding.php
@@ -7,16 +7,16 @@ namespace Orms\Util;
 class Encoding
 {
     /**
-     * Encodes the values of an array from utf8 to latin1.
+     * Encodes the values of an array from ISO-8859-1 (latin1) to UTF-8.
      * Also works on array of arrays or other nested structures.
      *
      */
-    public static function utf8_encode_recursive(mixed $data): mixed
+    public static function utf8_encode_recursive(mixed $data): mixed 
     {
-        if(is_string($data)) {
-            return utf8_encode($data);
-        }
-        elseif(is_array($data)) {
+        if (is_string($data)) {
+            return mb_convert_encoding($data, 'UTF-8', 'ISO-8859-1');
+        } 
+        elseif (is_array($data)) {
             return array_map(fn($x) => self::utf8_encode_recursive($x), $data);
         }
 
@@ -24,19 +24,19 @@ class Encoding
     }
 
     /**
-     * Decodes the values of an array from utf8 to latin1.
+     * Decodes the values of an array from UTF-8 to ISO-8859-1 (latin1).
      * Also works on array of arrays or other nested structures.
      *
      */
-    public static function utf8_decode_recursive(mixed $data): mixed
+    public static function utf8_decode_recursive(mixed $data): mixed 
     {
-        if(is_string($data)) {
-            return utf8_decode($data);
-        }
-        elseif(is_array($data)) {
+        if (is_string($data)) {
+            return mb_convert_encoding($data, 'ISO-8859-1', 'UTF-8');
+        } 
+        elseif (is_array($data)) {
             return array_map(fn($x) => self::utf8_decode_recursive($x), $data);
         }
-
+        
         return $data;
     }
 }

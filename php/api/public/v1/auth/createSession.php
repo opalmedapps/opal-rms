0cfdf5dc80eef8e7dcfb6a918a876e0828606d4b
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-05-15 19:29:45 +0000

    feat: orms auth to new backend
    (cherry picked from commit c4730d90a17db789eabe977968846ebf8f67c0d1)
    
    Co-authored-by: Anton Gladyr <anton.gladyr@muhc.mcgill.ca>

--- php/api/public/v1/auth/createSession.php
+++ php/api/public/v1/auth/createSession.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 require __DIR__."/../../../../../vendor/autoload.php";
 
+use GuzzleHttp\Cookie\SetCookie as CookieParser;
 use Orms\Authentication;
 use Orms\Http;
 use Orms\Util\Encoding;
@@ -29,10 +30,32 @@ $user = new class(
     ) {}
 };
 
-if(Authentication::validateUserCredentials($user->username, $user->password) === false) {
+$response = Authentication::login($user->username, $user->password);
+$content = json_decode($response->getBody()->getContents(), true);
+
+if ($response->getStatusCode() == 200 && isset($content["key"])) {
+    $cookie = Authentication::createUserSession($user->username);
+
+    // Iterate through all the "Set-Cookie" response headers and add them to the cookies.
+    foreach ($response->getHeaders() as $name => $values) {
+        # Skip if the header is not "Set-Cookie"
+        if ($name != 'Set-Cookie') continue;
+
+        if (in_array('csrftoken', $values)) {
+            $cookieParser = new CookieParser();
+            $setCookie = $cookieParser->fromString($name . ': ' . implode('; ', $values));
+            $cookie['csrftoken'] = $setCookie->getValue();
+        }
+
+        if (in_array('sessionid', $values)) {
+            $cookieParser = new CookieParser();
+            $setCookie = $cookieParser->fromString($name . ': ' . implode('; ', $values));
+            $cookie['sessionid'] = $setCookie->getValue();
+        }
+    }
+
+    Http::generateResponseJsonAndExit(200, data: $cookie);
+}
+else {
     Http::generateResponseJsonAndExit(406);
 }
-
-$cookie = Authentication::createUserSession($user->username);
-
-Http::generateResponseJsonAndExit(200, data: $cookie);

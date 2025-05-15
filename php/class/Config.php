ab2534993b358209ac9457f05de3a64e6b80f1f3
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-05-15 19:30:43 +0000

    feat: use .env for environment variables
    (cherry picked from commit 9cf3c669841388d4b369af38a197ae1b4271013b)
    
    Co-authored-by: Anton Gladyr <anton.gladyr@muhc.mcgill.ca>

--- php/class/Config.php
+++ php/class/Config.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Orms;
 
 use Exception;
+use Dotenv\Dotenv;
 
 Config::__init();
 
@@ -29,72 +30,99 @@ class Config
 
     public static function __init(): void
     {
-        $loadedData = parse_ini_file(__DIR__."/../../config/config.conf", true) ?: throw new Exception("Loading configs failed");
-        $parsedData = self::_parseData($loadedData);
+        $dotenv = Dotenv::createImmutable(__DIR__ . '/../..');
+        $dotenv->load();
+
+        // Ensure that the following environment variables are set
+        $dotenv->required('BASE_PATH')->notEmpty();
+        $dotenv->required('BASE_URL')->notEmpty();
+        $dotenv->required('IMAGE_PATH')->notEmpty();
+        $dotenv->required('IMAGE_URL')->notEmpty();
+        $dotenv->required('LOG_PATH')->notEmpty();
+        $dotenv->required('ORMS_PORT')->isInteger();
+        $dotenv->required('ORMS_DATABASE_USER')->notEmpty();
+        $dotenv->required('ORMS_DATABASE_PASSWORD')->notEmpty();
+        $dotenv->required('ORMS_DATABASE_HOST')->notEmpty();
+        $dotenv->required('ORMS_DATABASE_NAME')->notEmpty();
+        $dotenv->required('ORMS_DATABASE_PORT')->isInteger();
+        $dotenv->required('FIREBASE_URL')->notEmpty();
+        $dotenv->required('FIREBASE_SECRET')->notEmpty();
+        $dotenv->required('HIGHCHARTS_URL')->notEmpty();
+        $dotenv->required('OIE_ENABLED')->notEmpty();
+        $dotenv->required('SMS_ENABLED')->notEmpty();
+        $dotenv->required('SMS_REMINDER_CRON_ENABLED')->notEmpty();
+        $dotenv->required('SMS_INCOMING_SMS_CRON_ENABLED')->notEmpty();
+        $dotenv->required('NEW_OPAL_ADMIN_URL')->notEmpty();
+        $dotenv->required('NEW_OPAL_ADMIN_TOKEN')->notEmpty();
+        $dotenv->required('SEND_WEIGHTS')->notEmpty();
+        $dotenv->required('VWR_CRON_ENABLED')->notEmpty();
+        $dotenv->required('EMAIL')->notEmpty();
+
+        $_ENV = self::_parseData($_ENV);
 
         //create required configs
         $environment = new EnvironmentConfig(
-            basePath:                       $parsedData["path"]["BASE_PATH"],
-            baseUrl:                        $parsedData["path"]["BASE_URL"],
-            imagePath:                      $parsedData["path"]["IMAGE_PATH"],
-            firebaseUrl:                    $parsedData["path"]["FIREBASE_URL"],
-            firebaseSecret:                 $parsedData["path"]["FIREBASE_SECRET"],
-            completedQuestionnairePath:     $parsedData["path"]["BASE_PATH"]."/tmp/completedQuestionnaires.json",
-            highchartsUrl:                  $parsedData["path"]["HIGHCHARTS_URL"] ?? null,
+            basePath:                       $_ENV["BASE_PATH"],
+            baseUrl:                        $_ENV["BASE_URL"],
+            imagePath:                      $_ENV["IMAGE_PATH"],
+            firebaseUrl:                    $_ENV["FIREBASE_URL"],
+            firebaseSecret:                 $_ENV["FIREBASE_SECRET"],
+            completedQuestionnairePath:     $_ENV["BASE_PATH"]."/tmp/completedQuestionnaires.json",
+            highchartsUrl:                  $_ENV["HIGHCHARTS_URL"] ?? null,
         );
 
         $system = new SystemConfig(
-            emails:                             $parsedData["system"]["EMAIL"] ?? [],
-            sendWeights:                        (bool) ($parsedData["system"]["SEND_WEIGHTS"] ?? false),
-            vwrAppointmentCronEnabled:          (bool) ($parsedData["system"]["VWR_CRON_ENABLED"] ?? false),
-            appointmentReminderCronEnabled:     (bool) ($parsedData["system"]["SMS_REMINDER_CRON_ENABLED"] ?? false),
-            processIncomingSmsCronEnabled:      (bool) ($parsedData["system"]["INCOMING_SMS_CRON_ENABLED"] ?? false),
-            newOpalAdminUrl:                    $parsedData["opal"]["NEW_OPAL_ADMIN_URL"] ?? null,
-            newOpalAdminToken:                  $parsedData["opal"]["NEW_OPAL_ADMIN_TOKEN"] ?? null,
+            emails:                             explode(',', $_ENV["EMAIL"]),
+            sendWeights:                        (bool) ($_ENV["SEND_WEIGHTS"] ?? false),
+            vwrAppointmentCronEnabled:          (bool) ($_ENV["VWR_CRON_ENABLED"] ?? false),
+            appointmentReminderCronEnabled:     (bool) ($_ENV["SMS_REMINDER_CRON_ENABLED"] ?? false),
+            processIncomingSmsCronEnabled:      (bool) ($_ENV["SMS_INCOMING_SMS_CRON_ENABLED"] ?? false),
+            newOpalAdminUrl:                    $_ENV["NEW_OPAL_ADMIN_URL"] ?? null,
+            newOpalAdminToken:                  $_ENV["NEW_OPAL_ADMIN_TOKEN"] ?? null,
         );
 
         $ormsDb = new DatabaseConfig(
-            host:           $parsedData["database"]["ORMS_HOST"],
-            port:           $parsedData["database"]["ORMS_PORT"],
-            databaseName:   $parsedData["database"]["ORMS_DB"],
-            username:       $parsedData["database"]["ORMS_USERNAME"],
-            password:       $parsedData["database"]["ORMS_PASSWORD"],
+            host:           $_ENV["ORMS_DATABASE_HOST"],
+            port:           $_ENV["ORMS_DATABASE_PORT"],
+            databaseName:   $_ENV["ORMS_DATABASE_NAME"],
+            username:       $_ENV["ORMS_DATABASE_USER"],
+            password:       $_ENV["ORMS_DATABASE_PASSWORD"],
         );
 
         $logDb = new DatabaseConfig(
-            host:           $parsedData["database"]["LOG_HOST"],
-            port:           $parsedData["database"]["LOG_PORT"],
-            databaseName:   $parsedData["database"]["LOG_DB"],
-            username:       $parsedData["database"]["LOG_USERNAME"],
-            password:       $parsedData["database"]["LOG_PASSWORD"],
+            host:           $_ENV["LOG_DATABASE_HOST"],
+            port:           $_ENV["LOG_DATABASE_PORT"],
+            databaseName:   $_ENV["LOG_DATABASE_NAME"],
+            username:       $_ENV["LOG_DATABASE_USER"],
+            password:       $_ENV["LOG_DATABASE_PASSWORD"],
         );
 
         //create optional configs
 
-        if((bool) $parsedData["oie"]["ENABLED"] !== true) {
+        if((bool) $_ENV["OIE_ENABLED"] !== true) {
             $oie = null;
         }
         else {
             $oie = new OpalInterfaceEngineConfig(
-                oieUrl:    $parsedData["oie"]["OIE_URL"],
-                username:  $parsedData["oie"]["OIE_USERNAME"],
-                password:  $parsedData["oie"]["OIE_PASSWORD"]
+                oieUrl:    $_ENV["OIE_URL"],
+                username:  $_ENV["OIE_USERNAME"],
+                password:  $_ENV["OIE_PASSWORD"]
             );
         }
 
-        if((bool) $parsedData["sms"]["ENABLED"] !== true) {
+        if((bool) $_ENV["SMS_ENABLED"] !== true) {
             $sms = null;
         }
         else {
             $sms = new SmsConfig(
-                provider:                       $parsedData["sms"]["PROVIDER"],
-                licenceKey:                     $parsedData["sms"]["LICENCE_KEY"],
-                token:                          $parsedData["sms"]["TOKEN"] ?? "",
-                longCodes:                      $parsedData["sms"]["REGISTERED_LONG_CODES"] ?? [],
-                failedCheckInMessageEnglish:    $parsedData["sms"]["FAILED_CHECK_IN_MESSAGE_EN"],
-                failedCheckInMessageFrench:     $parsedData["sms"]["FAILED_CHECK_IN_MESSAGE_FR"],
-                unknownCommandMessageEnglish:   $parsedData["sms"]["UNKNOWN_COMMAND_MESSAGE_EN"],
-                unknownCommandMessageFrench:    $parsedData["sms"]["UNKNOWN_COMMAND_MESSAGE_FR"],
+                provider:                       $_ENV["SMS_PROVIDER"],
+                licenceKey:                     $_ENV["SMS_LICENCE_KEY"],
+                token:                          $_ENV["SMS_TOKEN"] ?? "",
+                longCodes:                      explode(',', $_ENV["REGISTERED_LONG_CODES"]),
+                failedCheckInMessageEnglish:    $_ENV["FAILED_CHECK_IN_MESSAGE_EN"],
+                failedCheckInMessageFrench:     $_ENV["FAILED_CHECK_IN_MESSAGE_FR"],
+                unknownCommandMessageEnglish:   $_ENV["UNKNOWN_COMMAND_MESSAGE_EN"],
+                unknownCommandMessageFrench:    $_ENV["UNKNOWN_COMMAND_MESSAGE_FR"],
             );
         }
 

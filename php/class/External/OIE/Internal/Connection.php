411063f1deec3aa5f16435a4b44c934b335c86fc
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-05-15 19:43:48 +0000

    add final explanations for remaining OIE endpoints
    (cherry picked from commit 27f143e5149c932cbe3b013c49894377a91441a6)
    
    Co-authored-by: Napparific <kelly.agnew@mail.mcgill.ca>

--- php/class/External/OIE/Internal/Connection.php
+++ php/class/External/OIE/Internal/Connection.php
@@ -9,8 +9,12 @@ use GuzzleHttp\Client;
 use Orms\Config;
 
 class Connection
-{
+{   
+    # These are the last 2 endpoints which currently go to the OIE
+
+    # Used when a patient checks in via kiosk, OIE forwards call to {serverAriaIE}/patient/hasPhoto and returns a boolean response
     public const API_ARIA_PHOTO                                 = "Patient/Photo";
+    # Used when a new patient measurement is created by a clinical staff, pdf submitted to OIE channel (same channel as questionnaire pdf from backend)
     public const API_MEASUREMENT_PDF                            = "report/post";
 
     public static function getHttpClient(): ?Client

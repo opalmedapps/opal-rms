de241a8f79799785fb98d428c5ef82e91e948370
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-05-15 19:36:58 +0000

    fix: add logLoginEvent function to record the logs
    (cherry picked from commit e4ab6fb91f0f126009f14f129d0866bef5eb690b)
    
    Co-authored-by: Shifeng Chen <shifeng.chen@muhc.mcgill.ca>

--- php/api/public/v1/auth/validateSession.php
+++ php/api/public/v1/auth/validateSession.php
@@ -4,14 +4,14 @@ declare(strict_types=1);
 
 require __DIR__."/../../../../../vendor/autoload.php";
 
-use GuzzleHttp\Cookie\SetCookie as CookieParser;
 use Orms\Authentication;
 use Orms\Http;
 use Orms\Util\Encoding;
 use Orms\System\Logger;
 
-//script to authenticate a user trying to log into ORMS
+//script to validate a user session logged into ORMS
 //creates a session on the server using memcache and returns the info needed for the front end to create a cookie that validates the user
+//log the information in success or failed
 
 try {
     $fields = Http::parseApiInputs('v1');
@@ -26,40 +26,27 @@ if(isset($_COOKIE["sessionid"])) {
     $sessionid = $_COOKIE["sessionid"];
 }
 
+$username = "";
+$first_name = "";
+$last_name = "";
 try {
     $response = Authentication::validate($sessionid);
     $content = json_decode($response->getBody()->getContents(), true);
     $username = $content["username"];
-    $error = "";
+    $first_name = $content["first_name"];
+    $last_name = $content["last_name"];
     if (
         $response->getStatusCode() == 200
         && isset($username)
     ) {
         // If authenticated and authorized, return HTTP 200 and set cookies
         $cookie = Authentication::createUserSession($username);
-
+        Logger::logLoginEvent($username, $first_name.' '.$last_name, $response->getStatusCode(), null);
         Http::generateResponseJsonAndExit(200, data: $cookie);
     }
-    elseif ($response->getStatusCode() == 403) {
-        // If user is unauthorized (HTTP 403) return a human-readable error message
-        $error = "You do not have permission to access ORMS. Contact the Opal systems administrator.";
-        Http::generateResponseJsonAndExit(
-            httpCode: 403,
-            error: $error
-        );
-    }
-    else {
-        // Return "Authentication failure" for any other errors
-        $error = "Authentication failure.";
-        Http::generateResponseJsonAndExit(
-            httpCode: 406,
-            error: $error
-        );
-    }
-
-    Logger::logLoginEvent($username, $response->getStatusCode(), $error);
 }
 catch(Exception $e) {
+    Logger::logLoginEvent($username, $first_name.' '.$last_name, $e->getCode(), $e->getMessage());
     Http::generateResponseJsonAndExit(
         httpCode: 403,
         error: $e->getMessage()

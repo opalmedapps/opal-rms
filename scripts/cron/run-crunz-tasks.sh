10d78f7f007efea0ce8ec75369f54f43205e98d9
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-05-15 19:31:28 +0000

    chore: add comments
    (cherry picked from commit 5e941746dde3b4c6c31da4b674eba265aa34387d)
    
    Co-authored-by: Anton Gladyr <anton.gladyr@muhc.mcgill.ca>

--- scripts/cron/run-crunz-tasks.sh
+++ scripts/cron/run-crunz-tasks.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+# The Crunz binary should be executed from the ./config directory so it uses the configurations set in the crunz.yml
 cd /var/www/orms/config
 /usr/local/bin/php ./../vendor/bin/crunz schedule:run 2>>/var/log/cron_errors.log
